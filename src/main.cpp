2d7684052f3742c758d0cd14fc7d3f0d7fb028e1
neltu <dcastilloa@utem.cl>
2023-12-08 02:20:37 -0300

    Recolección por bloques nodo y proceso

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <vector>
 #include <mpi.h>
+#include <limits>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image_write.h"
@@ -32,7 +33,26 @@ int main(int argc, char* argv[]) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Obtener el número de nodos del archivo de host
+    int num_nodes;
+    std::ifstream hostfile("maquinas.txt");
+    if (!hostfile) {
+        std::cerr << "Error al abrir el archivo de host (maquinas.txt)\n";
+        MPI_Finalize();
+        return 1;
+    }
+
+    std::vector<std::string> nodes;
+    std::string node;
+    while (hostfile >> node) {
+        nodes.push_back(node);
+    }
+
+    num_nodes = nodes.size();
+
     auto start_time = std::chrono::high_resolution_clock::now();
+
     if (argc != 6) {
         if (rank == 0) {
             std::cerr << "Uso: " << argv[0] << " alfa.txt rojo.txt verde.txt azul.txt promedio.txt\n";
@@ -44,7 +64,6 @@ int main(int argc, char* argv[]) {
     // Calcular la cantidad de filas que cada proceso MPI procesará
     int rows_per_process = height / size;
     int start_row = rank * rows_per_process;
-    int end_row = start_row + rows_per_process;
 
     // Abrir los archivos directamente con los nombres obtenidos de argv
     std::ifstream alfaFile(argv[1]), rojoFile(argv[2]), verdeFile(argv[3]), azulFile(argv[4]), promedioFile(argv[5]);
@@ -64,8 +83,9 @@ int main(int argc, char* argv[]) {
         promedioFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
-    // Crear un solo vector para almacenar los píxeles de todos los canales
-    std::vector<unsigned char> imagePixels(width * channels * rows_per_process);
+    // Crear un solo vector para almacenar los bloques de píxeles de todos los canales
+    std::vector<unsigned char> imageBlocks(width * channels * rows_per_process);
+
     std::string alfaValue, rojoValue, verdeValue, azulValue, promedioValue;
     alfaValue.reserve(3);  // Ajusta el tamaño según la longitud máxima esperada
     rojoValue.reserve(3);
@@ -76,6 +96,9 @@ int main(int argc, char* argv[]) {
     // Leer los valores de los archivos y almacenarlos directamente en el vector
     for (int i = 0; i < rows_per_process; ++i) {
         for (int j = 0; j < width; ++j) {
+            // Calcular el índice local en el bloque del proceso actual
+            int localIndex = (i * width + j) * channels;
+
             alfaFile >> alfaValue;
             rojoFile >> rojoValue;
             verdeFile >> verdeValue;
@@ -83,14 +106,13 @@ int main(int argc, char* argv[]) {
             promedioFile >> promedioValue;
 
             try {
-                int index = (i * width + j) * channels;
-
-                imagePixels[index]     = (rojoValue == "*") ? static_cast<unsigned char>(std::stoi(promedioValue) * 0.3) : static_cast<unsigned char>(std::stoi(rojoValue));
-                imagePixels[index + 1] = (verdeValue == "*") ? static_cast<unsigned char>(std::stoi(promedioValue) * 0.59) : static_cast<unsigned char>(std::stoi(verdeValue));
-                imagePixels[index + 2] = (azulValue == "*") ? static_cast<unsigned char>(std::stoi(promedioValue) * 0.11) : static_cast<unsigned char>(std::stoi(azulValue));
-                imagePixels[index + 3] = static_cast<unsigned char>(std::stoi(alfaValue));
+                // Calcular el píxel directamente en el bloque del proceso actual
+                imageBlocks[localIndex]     = (rojoValue == "*") ? static_cast<unsigned char>(std::stoi(promedioValue) * 0.3) : static_cast<unsigned char>(std::stoi(rojoValue));
+                imageBlocks[localIndex + 1] = (verdeValue == "*") ? static_cast<unsigned char>(std::stoi(promedioValue) * 0.59) : static_cast<unsigned char>(std::stoi(verdeValue));
+                imageBlocks[localIndex + 2] = (azulValue == "*") ? static_cast<unsigned char>(std::stoi(promedioValue) * 0.11) : static_cast<unsigned char>(std::stoi(azulValue));
+                imageBlocks[localIndex + 3] = static_cast<unsigned char>(std::stoi(alfaValue));
             } catch (const std::exception& e) {
-                std::cerr << "Error convirtiendo a entero en posición (" << i << ", " << j << "): " << e.what() << std::endl;
+                std::cerr << "Error convirtiendo a entero en posición (" << i << ", " << j << "): " << e.what() << '\n';
                 MPI_Finalize();
                 return 1;  // Terminar el programa debido al error
             }
@@ -100,28 +122,28 @@ int main(int argc, char* argv[]) {
     // Cerrar los archivos
     closeFiles(alfaFile, rojoFile, verdeFile, azulFile, promedioFile);
 
-    // Crear un vector para almacenar los píxeles recopilados en el proceso 0
+    // Recopilar solo los bloques de píxeles de todos los nodos al proceso 0
     std::vector<unsigned char> receivedPixels(width * channels * rows_per_process * size);
-
-    // Enviar la información al proceso 0 para que combine las imágenes
-    MPI_Gather(imagePixels.data(), imagePixels.size(), MPI_UNSIGNED_CHAR,
-               receivedPixels.data(), imagePixels.size(), MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Gather(imageBlocks.data(), imageBlocks.size(), MPI_UNSIGNED_CHAR,
+               receivedPixels.data(), imageBlocks.size(), MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
     // Obtener el tiempo de finalización
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-    std::cout << "Proceso " << rank << ": Tiempo transcurrido: " << duration.count() << " milisegundos\n";
+    std::cout << "Proceso " << rank << " en Nodo " << rank % num_nodes << ": Tiempo transcurrido: " << duration.count() << " milisegundos\n";
 
     // Liberar la memoria
     MPI_Finalize();
 
-    // Combina las imágenes en el proceso 0 y genera el archivo final
+    // Proceso 0 combina los bloques de píxeles y genera el archivo final
     if (rank == 0) {
         combineImages(receivedPixels, "combined_image.png");
     }
 
     return 0;
 }
+
+
 
 
 
